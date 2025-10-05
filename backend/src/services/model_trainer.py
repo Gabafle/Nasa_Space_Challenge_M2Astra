2f5a0231b8afb79670e226bc2d93567eb29993dd
Rya <mat.no.spam@gmail.com>
2025-10-05 14:37:29 +0200

    quickfix xgboost

--- backend/src/services/model_trainer.py
+++ backend/src/services/model_trainer.py
@@ -1,6 +1,6 @@
 import numpy as np 
 from backend.src.services.model_db_manager import ModelDataBaseManager
-
+from backend.src.models.base_model import BaseModel
 
 ## TODOOOOOOOO
 
@@ -9,5 +9,5 @@ class ModelTrainer:
     def __init__(self):
         pass
     
-    def train(self, latest_model, X: np.ndarray, y: np.ndarray):
+    def train(self, latest_model: BaseModel, X: np.ndarray, y: np.ndarray):
         pass 
